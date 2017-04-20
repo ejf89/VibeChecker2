17d851615905ac021c3f86546efccc99f92ec066
github username <github email address>
2017-04-20 19:34:44 -0400

    added vibecounts to match table and yes no options to view table

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,27 +1,31 @@
 class Search < ActiveRecord::Base
     has_many :tweets
-    
+
     def self.search_compare
       current = Search.last.search
       matches = Search.where(search: current)
       if matches.length >= 2
 
         puts "This vibe has been checked before. Would u like to see previous checks of: #{current}?"
+        puts "             *****(y/n)*****"
 
         if gets.chomp == 'y'
             rows1 = ['VIBE']
             rows2 = ['SCORE']
+            rows3 = ['# of Vibes']
             stamps = []
 
             matches.each do |match|
               rows1 << [match.vibe]
               rows2 << [match.score]
+              rows3 << [match.count]
               stamps << match.created_at.to_s
             end
 
             table = Terminal::Table.new :headings => ['HELLO', 'NOW', 'THEN'] do |t|
               t << rows1.flatten
               t.add_row rows2.flatten
+              t.add_row rows3.flatten
           end
             puts table
         end
