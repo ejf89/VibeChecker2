6c123aa607d71732b4f402791df6210339cb1205
github username <github email address>
2017-04-20 16:23:57 -0400

    pre blog

--- app/adapter/sentimentchecker.rb
+++ app/adapter/sentimentchecker.rb
@@ -6,12 +6,16 @@ class SentimentChecker < Sentimental
     end
 
     def self.declaration(average)
-        if average < (-1 * 0.1)
+        if average < (-1 * 0.7)
+            "very bad"
+        elsif average < (-1 * 0.1)
             "bad"
+        elsif average > 0.7
+            "very good"
         elsif average > 0.05
             "good"
         else
-            "indifferent"
+            "smoothly indifferent"
         end
     end
 
